f9bdc880eab3caecca920c8e93fdbabfab9d63a4
Marcelo Oliveira <martins.oliveira.mo@gmail.com>
2018-06-07 17:14:08 -0300

    Remove queue from ph
    Signed-off-by: Marcelo Oliveira <martins.oliveira.mo@gmail.com>

--- ph_server.py
+++ ph_server.py
@@ -1,21 +1,16 @@
 from nameko.rpc import rpc
-from nameko.messaging import Publisher
-from kombu import Exchange, Queue
 import db
 from db import Ph
 
-exchange = Exchange("main", "direct", durable=True)
-queue = Queue("ph_queue", exchange=exchange)
 
 class PhServer():
     name = "ph_server"
-    publish = Publisher(exchange=exchange, queue=queue)
 
     @rpc
     def receive_ph(self, ph):
+        ph = round(ph, 1)
         p = Ph()
         p.value = ph
         db.session.add(p)
         db.session.commit()
-        self.publish(ph)
         return ph
